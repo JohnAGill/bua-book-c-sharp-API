882048afc69ecae0d26426653673c416669e995e
JohnAGill <johnagill95@gmail.com>
2016-06-07 15:28:09 +0100

    added code for logging player name

---
+++ bua-book-c-sharp-API/bua-book-c-sharp-API/PlayersResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bua_book_c_sharp_API
+{
+    public class PlayersResponse
+    {
+        public PlayersResponse1 response { get; set; }
+
+        public List<Player> GetPlayers()
+        {
+            return response.players;
+        }
+    }
+
+    public class PlayersResponse1
+    {
+        public List<Player> players { get; set; }
+    }
+
+    public class Player
+    {
+        public string ticker { get; set; }
+
+        public int player_id { get; set; }
+
+        public string name { get; set; }
+    }
+}
--- bua-book-c-sharp-API/bua-book-c-sharp-API/Program.cs
+++ bua-book-c-sharp-API/bua-book-c-sharp-API/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
 
 namespace bua_book_c_sharp_API
 {
@@ -10,7 +11,12 @@ namespace bua_book_c_sharp_API
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            RestClient client = new RestClient("https://dev-api.buabook.com/");
+            RestRequest request = new RestRequest("api/players?user_id=0&token=00000000-0000-0000-0000-000000000000", Method.GET);
+            PlayersResponse queryResult = client.Execute<PlayersResponse>(request).Data;
+            List<Player> players = queryResult.GetPlayers();
+            Player ozil = players.Find((p) => p.ticker == "OZL.AR");
+            Console.WriteLine(ozil.name);
             System.Threading.Thread.Sleep(5000);
         }
     }
